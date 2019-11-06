95d76327c1f7567c8f1dfbadfe4ee8c47414a567
vihangmirkhelkar <vihang10@csu.fullerton.edu>
2019-11-06 14:47:22 -0800

    Handle validation Errors

--- middlewares/error.js
+++ middlewares/error.js
@@ -12,6 +12,10 @@ const errorHandler = (err, req, res, next) => {
         const message = 'Duplicate field values entered';
         error = new ErrorResponse(message, 400);
     }
+    if (err.name === 'ValidationError') {
+        const message = Object.values(err.errors).map(val => val.message);
+        error = new ErrorResponse(message, 400);
+    }
     res.status(error.statusCode || 500).json( { success: false, error: error.message || 'Server Error'});
 };
 
